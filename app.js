4a7dc4c50f7109c2ac16e9b2aeffecacdfd00eaa
Ava <avanbtech@gmail.com>
2020-07-24 15:17:34 -0700

    Update income and expenses in UI controller

--- app.js
+++ app.js
@@ -92,7 +92,11 @@ var UIController = (function() {
 		inputValue: '.add__value',
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
-		expenseContainer: '.expenses__list'
+		expenseContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage'
 	};
 
 	return {
@@ -141,6 +145,18 @@ var UIController = (function() {
 			});
 		},
 
+		displayBudget: function(obj) {
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+			} else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
+		},
+
 		getDOMstrings: function() {
 			return DOMstrings;
 		}
@@ -167,7 +183,7 @@ var controller = (function(budgetCtr, UICtr) {
 		var budget = budgetController.getBudget();
 
 		// Display the budget on the UI
-		console.log(budget);
+		UICtr.displayBudget(budget);
 	};
 
 	// This get called when user press Enter
@@ -194,6 +210,12 @@ var controller = (function(budgetCtr, UICtr) {
 
 	return {
 		init: function() {
+			UICtr.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1
+			});
 			setupEventListener();
 		}
 	};
